314fd67f79423e4e99a741c24b3020c3b46fb2bd
Toby Penner <tobypenner01@gmail.com>
2026-02-23 17:40:15 -0500

    Lower reqs

--- slurm/environment_smoke.sh
+++ slurm/environment_smoke.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=environment_smoke
 #SBATCH --output=logs/%j.out
 #SBATCH --partition=gpu-short
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=128gb
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=32gb
 #SBATCH --gres=gpu:rtxa6000:1
 #SBATCH --time=30:00
 

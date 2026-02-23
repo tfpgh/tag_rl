6a6c36aaa2551bf257f50cfee45da74a437a2da6
Toby Penner <tobypenner01@gmail.com>
2026-02-23 15:33:34 -0500

    Increase envs a lot

--- environment/environment.py
+++ environment/environment.py
@@ -425,7 +425,7 @@ if __name__ == "__main__":
     )
 
     # Vectorized test
-    N = 256
+    N = 65_536
     print(f"\nVectorized ({N} envs)...")
     v_reset = jax.vmap(env.reset)
     v_step = jax.vmap(env.step)
@@ -447,7 +447,7 @@ if __name__ == "__main__":
     print(f"  first step (incl JIT): {time.time() - t0:.1f}s")
 
     # Benchmark post-JIT
-    n_iters = 20
+    n_iters = 10
     t0 = time.time()
     with jax.profiler.trace("tensorboard/"):
         for _ in tqdm(range(n_iters)):

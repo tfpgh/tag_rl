7db8e973628f3d733bc24210e0c782bd9dcd6e50
Toby Penner <tobypenner01@gmail.com>
2026-02-23 11:16:21 -0500

    Create environment_smoke.sh

---
+++ slurm/environment_smoke.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=environment_smoke
+#SBATCH --output=logs/%j.out
+#SBATCH --partition=gpu-short
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128gb
+#SBATCH --gres=gpu:rtxa6000:1
+#SBATCH --time=30:00
+
+uv run -m environment.environment

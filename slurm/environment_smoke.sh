a3506ba3ed39c80a0f2033c428f702a1e7195f8c
Toby Penner <tobypenner01@gmail.com>
2026-02-23 17:40:43 -0500

    Change node

--- slurm/environment_smoke.sh
+++ slurm/environment_smoke.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=environment_smoke
 #SBATCH --output=logs/%j.out
 #SBATCH --partition=gpu-short
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=32gb
-#SBATCH --gres=gpu:rtxa6000:1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128gb
+#SBATCH --gres=gpu:rtxa5000:1
 #SBATCH --time=30:00
 
 export TQDM_MININTERVAL=15

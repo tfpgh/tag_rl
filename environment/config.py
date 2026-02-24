db17b5896ef5fece30dfb1a583e5554d97f35ff1
Toby Penner <tobypenner01@gmail.com>
2026-02-24 13:36:36 -0500

    Tweaks to env

--- environment/config.py
+++ environment/config.py
@@ -11,7 +11,7 @@ class EnvironmentConfig:
     agent_z: float = 0.0299  # Chassis center height
 
     # Agent radius multiplier for tag distance
-    tag_distance_factor: float = 1.1
+    tag_distance_factor: float = 1.03
 
     # Perception rays
     n_rays: int = 64

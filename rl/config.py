9469f8811fcc0369133eef509a1b53be7b6a7db0
Toby Penner <tobypenner01@gmail.com>
2026-02-24 16:51:48 -0500

    Add RL config

---
+++ rl/config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class RLConfig:
+    num_envs: int = 2048
+    num_steps: int = 128
+
+    total_timesteps: int = int(5e7)
+
+    lr: float = 3e-4
+    update_epochs: int = 4
+    num_minibatches: int = 8
+    gamma: float = 0.99
+    gae_lambda: float = 0.95
+    clip_eps: float = 0.2
+    ent_coef: float = 0.01
+    vf_coef: float = 0.5
+    max_grad_norm: float = 0.5

9b21a7f50af17bbcc7323a86bed0b7ae6dfbbd48
Toby Penner <tobypenner01@gmail.com>
2026-02-23 19:31:17 -0500

    Reduce nconmax

--- environment/environment.py
+++ environment/environment.py
@@ -237,7 +237,7 @@ class TagEnvironment:
 
         qvel = jnp.zeros(self.mj_model.nv)
 
-        mjx_data = mjx.make_data(self.mj_model, impl="warp", nconmax=20 * self.n_envs)
+        mjx_data = mjx.make_data(self.mj_model, impl="warp", nconmax=11 * self.n_envs)
         mjx_data = mjx_data.replace(qpos=qpos, qvel=qvel)
         mjx_data = mjx.forward(self.mjx_model, mjx_data)
 

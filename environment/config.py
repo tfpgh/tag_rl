176d1624690e0fdae5da98385a74e1b9ba1f03f8
Toby Penner <tobypenner01@gmail.com>
2026-02-23 19:33:53 -0500

    Scale up ray count

--- environment/config.py
+++ environment/config.py
@@ -14,7 +14,7 @@ class EnvironmentConfig:
     tag_distance_factor: float = 1.1
 
     # Perception rays
-    n_rays: int = 32
+    n_rays: int = 64
 
     # Action frequency, hz
     action_frequency: int = 20

c3c440ec8e5e5379c6131820980e0df2f3165111
Toby Penner <tobypenner01@gmail.com>
2026-02-25 09:12:03 -0500

    Update hyperparams

--- rl/config.py
+++ rl/config.py
@@ -6,18 +6,18 @@ class RLConfig:
     num_envs: int = 1024
     num_steps: int = 128
 
-    total_timesteps: int = int(1e9)
+    total_timesteps: int = int(2e8)
 
     hidden_size: int = 128
 
     lr: float = 3e-4
     anneal_lr: bool = True
     update_epochs: int = 4
-    num_minibatches: int = 64
+    num_minibatches: int = 32
     gamma: float = 0.99
     gae_lambda: float = 0.95
     clip_eps: float = 0.2
-    ent_coef: float = 0.015
+    ent_coef: float = 0.01
     vf_coef: float = 0.5
     max_grad_norm: float = 0.5
 

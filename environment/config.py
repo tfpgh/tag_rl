1a7dc09643251be407d3dd1ef2e6d2054f1455ec
Toby Penner <tobypenner01@gmail.com>
2026-02-28 16:50:17 -0500

    Update config.py

--- environment/config.py
+++ environment/config.py
@@ -11,7 +11,7 @@ class EnvironmentConfig:
     agent_z: float = 0.0299  # Chassis center height
 
     # Agent radius multiplier for tag distance
-    tag_distance_factor: float = 1.03
+    tag_distance_factor: float = 1.05
 
     # Perception rays
     n_rays: int = 256
@@ -37,5 +37,5 @@ class EnvironmentConfig:
     wall_margin_factor: float = 1.5  # Minimum distance from wall (* agent_radius)
 
     # Obstacles
-    max_obstacles: int = 8  # always allocated in MJCF
-    obstacle_width: float = 0.2  # square box side length (meters)
+    max_obstacles: int = 10  # always allocated in MJCF
+    obstacle_width: float = 0.3  # square box side length (meters)

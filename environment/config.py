c5f213fe075cc0c7c3b1400ec1becd7b76a7b85b
Toby Penner <tobypenner01@gmail.com>
2026-03-01 12:09:52 -0500

    New reward shaping

--- environment/config.py
+++ environment/config.py
@@ -24,8 +24,8 @@ class EnvironmentConfig:
     chaser_freeze_seconds: int = 5
 
     # Rewards
-    win_reward: float = 0.05  # timeout for evader, tag for chaser
-    time_reward: float = 0.001  # + for evader, - for chaser
+    win_reward: float = 1.0  # timeout for evader, tag for chaser
+    time_reward: float = 0.004  # + for evader, - for chaser
     distance_shaping_scale: float = 0.00
     distance_shaping_gamma: float = 0.99
 

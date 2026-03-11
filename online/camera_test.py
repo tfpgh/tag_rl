f7b13ed70a5972db09412a4708b71a5ecfa24523
Toby Penner <tobypenner01@gmail.com>
2026-03-11 19:06:50 -0400

    Update camera_test.py

--- online/camera_test.py
+++ online/camera_test.py
@@ -1,21 +1,17 @@
-import subprocess
-
 import cv2
-
-DEVICE = "/dev/video0"
 
 cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 
 # MJPG format
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-cap.set(cv2.CAP_PROP_FPS, 30)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FPS, 60)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
 # Manual exposure
-subprocess.run(["v4l2-ctl", "-d", DEVICE, "--set-ctrl=exposure_auto=1"])
-subprocess.run(["v4l2-ctl", "-d", DEVICE, "--set-ctrl=exposure_absolute=10"])
+cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+cap.set(cv2.CAP_PROP_EXPOSURE, 10)
 
 while True:
     ret, frame = cap.read()

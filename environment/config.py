e5f7d6b3b9f9f2966cbb0a53e0560693983552f1
Toby Penner <tobypenner01@gmail.com>
2026-03-12 09:36:28 -0400

    Update config.py

--- environment/config.py
+++ environment/config.py
@@ -11,7 +11,7 @@ class EnvironmentConfig:
     agent_z: float = 0.0299  # Chassis center height
 
     # Agent radius multiplier for tag distance
-    tag_distance_factor: float = 1.03
+    tag_distance_factor: float = 1.02
 
     # Perception rays
     n_rays: int = 256
@@ -21,14 +21,14 @@ class EnvironmentConfig:
 
     # Episode timing, seconds
     episode_max_length: int = 20
-    chaser_freeze_seconds: int = 5
+    chaser_freeze_seconds: int = 4
 
     # Rewards
     win_reward: float = 1.0  # timeout for evader, tag for chaser
-    time_reward: float = 0.005  # + for evader, - for chaser
+    time_reward: float = 0.006  # + for evader, - for chaser
     distance_shaping_scale: float = 0.00
     distance_shaping_gamma: float = 0.99
-    collision_penalty: float = 1.0  # Per-step penalty for wall/obstacle contact
+    collision_penalty: float = 2.0  # Per-step penalty for wall/obstacle contact
 
     # Normalization, not exact
     agent_max_linear_velocity: float = 1.35  # m/s
@@ -37,8 +37,8 @@ class EnvironmentConfig:
     minimum_starting_separation: float = 0.13  # m, center to center
     wall_margin_factor: float = 1.5  # Minimum distance from wall (* agent_radius)
 
-    collision_epsilon_factor: float = 1.05
+    collision_epsilon_factor: float = 1.04
 
     # Obstacles
-    max_obstacles: int = 7  # always allocated in MJCF
-    obstacle_width: float = 0.2  # square box side length (meters)
+    max_obstacles: int = 10  # always allocated in MJCF
+    obstacle_width: float = 0.1  # square box side length (meters)

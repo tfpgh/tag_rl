ccdc399539fb8f1160f74f761ea0f56ce86ebb8e
Toby Penner <tobypenner01@gmail.com>
2026-04-14 18:49:27 -0400

    New sysid + forwards data

--- slurm/sysid.sh
+++ slurm/sysid.sh
@@ -13,4 +13,4 @@ export PYTHONUNBUFFERED=1
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
 export XLA_FLAGS=--xla_gpu_triton_gemm_any=true
 
-uv run -m sysid.fit --data-root data --output-dir runs/sysid_$SLURM_JOB_ID --generations 10000 --population-size 256
+uv run -m sysid.fit --data-root data --output-dir runs/sysid_$SLURM_JOB_ID --generations 10000 --population-size 2048

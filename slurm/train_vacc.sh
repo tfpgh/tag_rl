0ae0e9b8bb6a097bdb33a8c8d92ed568a76cdc6b
Toby Penner <tobypenner01@gmail.com>
2026-04-20 22:14:43 -0400

    Add VACC slurm script

---
+++ slurm/train_vacc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=tag_train
+#SBATCH --output=logs/%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --partition=nvgpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=1-00:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32gb
+#SBATCH --gpus=8
+#SBATCH --constraint=GPU_SKU:RTX6000
+
+
+export TQDM_MININTERVAL=15
+export PYTHONUNBUFFERED=1
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
+export XLA_FLAGS=--xla_gpu_triton_gemm_any=true
+
+uv run -m rl.train --num-devices 8

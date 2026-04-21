267db1f82ac8443cc0637dc26e3f8e03af197c84
Toby Penner <tobypenner01@gmail.com>
2026-04-21 19:16:39 -0400

    Create render_vacc.sh

---
+++ slurm/render_vacc.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=tag_render
+#SBATCH --output=logs/%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --partition=nvgpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=10:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32gb
+#SBATCH --gpus=1
+
+
+export TQDM_MININTERVAL=15
+export PYTHONUNBUFFERED=1
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
+export XLA_FLAGS=--xla_gpu_triton_gemm_any=true
+
+uv sync --extra cuda13
+
+uv run -m rl.render
